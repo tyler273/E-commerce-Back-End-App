a8b5516288b568aa35934568b8f5df8e100a1168
Tyler <t.woods95@aol.com>
2023-06-17 19:23:29 -0400

    Get ID routes for tag, category and product working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product}]
+      // include: [{ model: Product }]
     });
     res.status(200).json(categoryData);
   } catch (err) {
@@ -16,10 +16,24 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.get('/:id', (req, res) => {
-//   // find one category by its `id` value
-//   // be sure to include its associated Products
-// });
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try { 
+    const categoryData = await Category.findByPk(req.params.id, {
+      // include: [{ model: Category }]
+    });
+
+  if (!categoryData) {
+    res.status(404).json({ message: "No product found with that id!" });
+    return;
+  }
+
+  res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // router.post('/', (req, res) => {
 //   // create a new category
