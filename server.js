0f873507bf0e7f496b562d8b06d472d225468d5e
Tyler <t.woods95@aol.com>
2023-06-18 00:24:56 -0400

    Changed port due to error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sequelize = require('./config/connection');
 // import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3006;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
