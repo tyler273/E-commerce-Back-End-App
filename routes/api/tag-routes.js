f5f3045b90565963bfd43a7d034c24347a1b6329
Tyler <t.woods95@aol.com>
2023-06-18 00:07:33 -0400

    Updated tag route put to fix previous error

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -39,7 +39,7 @@ router.post('/', async (req, res) => {
   // create a new tag
   try {
     const tagData = await Tag.create(req.body);
-    res.status(200).json(tag);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
   }
