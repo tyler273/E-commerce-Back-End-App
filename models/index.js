23e0352b2cc79ef81f13db4af2e93424e9a65019
Tyler <t.woods95@aol.com>
2023-06-17 23:00:56 -0400

    fixed issue by updating code in models folder index js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag 
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
 
 module.exports = {
   Product,
